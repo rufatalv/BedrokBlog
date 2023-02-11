a6e0ee9b31250779d165a508a9df97febbbf96d6
Rufat Aliyev <rufat845@gmail.com>
2023-02-11 18:33:57 +0400

    commit

--- src/components/PostItems.jsx
+++ src/components/PostItems.jsx
@@ -3,7 +3,7 @@ import PostItem from "./PostItem";
 
 const PostItems = ({ data }) => {
   return (
-    <div className=" flex flex-wrap gap-6">
+    <div className=" flex flex-wrap md:justify-between lg:justify-start gap-6">
       {data.map((item, idx) => (
         <PostItem data={item} key={idx} />
       ))}
